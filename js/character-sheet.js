15349125a9cb13a7b431d3212c94fc3818a65747
Denali Therrien <denali.therrien@nutrien.com>
2025-12-18 12:00:38 -0600

    getting started

---
+++ js/character-sheet.js
@@ -0,0 +1,18 @@
+import {OmnisearchBacking} from "./omnisearch/omnisearch-backing.js";
+
+const UP = "UP";
+const RIGHT = "RIGHT";
+const LEFT = "LEFT";
+const DOWN = "DOWN";
+const AX_X = "AXIS_X";
+const AX_Y = "AXIS_Y";
+
+const EVT_NAMESPACE = ".character_sheet";
+
+const TITLE_LOADING = "Loading...";
+
+class CharacterSheetBoard {
+    constructor () {
+
+    }
+}
